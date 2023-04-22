d023783ddc434bf7d82b201735a0ad2728a7dc35
AnKom13 <69415434+AnKom13@users.noreply.github.com>
2023-04-22 14:04:45 +0300

    Add files via upload

---
+++ NewsPaper/news/urls.py
@@ -0,0 +1,16 @@
+from django.urls import path, include
+from news.views import PostList
+#from news.views import PostDetail
+from news.views import detail
+
+urlpatterns = [
+    path('', PostList.as_view()),
+    # pk — это первичный ключ товара, который будет выводиться у нас в шаблон
+    # int — указывает на то, что принимаются только целочисленные значения
+# вариант вывода через адрес ex: http://127.0.0.1:8000/news/3
+#    path('<int:pk>', PostDetail.as_view()),
+
+# Вариант со списком и ссылками на элементы
+# внимание на параметр name. Без него django не может найти функцию
+    path('<int:pk>', detail, name='detail'),
+]
